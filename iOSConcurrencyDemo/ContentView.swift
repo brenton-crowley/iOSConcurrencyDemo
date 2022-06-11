76f3586aa9d5f47cc1b5bda70846688006d02ccd
brenton-crowley <brenton.crowley@gmail.com>
2022-06-11 14:39:01 +1000

    Initial Commit

---
+++ iOSConcurrencyDemo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  iOSConcurrencyDemo
+//
+//  Created by Brent Crowley on 11/6/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ iOSConcurrencyDemo/iOSConcurrencyDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOSConcurrencyDemoApp.swift
+//  iOSConcurrencyDemo
+//
+//  Created by Brent Crowley on 11/6/2022.
+//
+
+import SwiftUI
+
+@main
+struct iOSConcurrencyDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
